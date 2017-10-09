ed7648c031d4a2ed8e229f93efe903ad4490aa19
kr28mevada <32267196+kr28mevada@users.noreply.github.com>
2017-10-09 12:21:40 +0530

    Update Constants.java

--- CircleCITest/src/main/java/com/android/testBase/Constants.java
+++ CircleCITest/src/main/java/com/android/testBase/Constants.java
@@ -6,6 +6,6 @@ public class Constants
 	public static final String ACCESS_KEY = "b7219edb-02bd-4c37-badb-4d14594615d4";
 	public static final String Url = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
 	public static final String App_Source = "sauce-storage:Login.apk";
-	public static final String Device_Name = "Samsung Galaxy S4 Emulator";
-	public static final String Platform_Version = "4.3";
+	public static final String Device_Name = "Android GoogleAPI Emulator";
+	public static final String Platform_Version = "6.0";
 }
