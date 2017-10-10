e307eb2e53f46f7bf6890658693aee0fc571e564
kr28mevada <32267196+kr28mevada@users.noreply.github.com>
2017-10-10 13:03:51 +0530

    Update Constants.java

--- CircleCITest/src/main/java/com/android/testBase/Constants.java
+++ CircleCITest/src/main/java/com/android/testBase/Constants.java
@@ -5,7 +5,7 @@ public class Constants
 	public static final String USERNAME = "Kr28Mevada";
 	public static final String ACCESS_KEY = "b7219edb-02bd-4c37-badb-4d14594615d4";
 	public static final String Url = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
-	public static final String App_Source = "sauce-storage:Mynew.apk";
+	public static final String App_Source = "sauce-storage:Newupload.apk";
 	//public static final String App_Source = "https://136-104833988-gh.circle-artifacts.com/0/tmp/circle-artifacts.l31Ovk9/outputs/apk/app-debug.apk";
 	//public static final String App_Source = "http://saucelabs.com/example_files/ContactManager.apk";
 	public static final String Device_Name = "Android GoogleAPI Emulator";
