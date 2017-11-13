afece260cd57962442c312f6e6e76dc72849e55b
kr28mevada <32267196+kr28mevada@users.noreply.github.com>
2017-11-13 15:41:07 +0530

    Update Constants.java

--- CircleCITest/src/main/java/com/android/testBase/Constants.java
+++ CircleCITest/src/main/java/com/android/testBase/Constants.java
@@ -2,8 +2,8 @@ package com.android.testBase;
 
 public class Constants 
 {
-	public static final String USERNAME = "Kr28Mevada";
-	public static final String ACCESS_KEY = "b7219edb-02bd-4c37-badb-4d14594615d4";
+	public static final String USERNAME = "akhil_rj";
+	public static final String ACCESS_KEY = "e9dcb689-0ea0-489e-8e85-c93de2ce7f65";
 	public static final String Url = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
 	public static final String App_Source = "sauce-storage:Newupload.apk";
 	//public static final String App_Source = "https://136-104833988-gh.circle-artifacts.com/0/tmp/circle-artifacts.l31Ovk9/outputs/apk/app-debug.apk";
